99848e89cadb69b1b9e5d7ad3142304773a71760
Amol Jagtap <ajfsdeveloper@gmail.com>
2021-04-04 13:46:44 +0530

    IMAGE_API: Send simple image file as response

--- src/routes/images/index.ts
+++ src/routes/images/index.ts
@@ -1,10 +1,17 @@
 import express from 'express'
+import fs from 'fs'
 import { imageParamsValidator } from '../../middlewares'
 
 const imagesRoutes = express.Router()
 
 imagesRoutes.get('/', imageParamsValidator, (req, res) => {
-  res.send('Images Route...')
+  const file = 'src/assets/images/fjord.jpg'
+
+  const img = fs.readFileSync(file)
+
+  res.type('jpg')
+
+  res.send(img)
 })
 
 export { imagesRoutes }
