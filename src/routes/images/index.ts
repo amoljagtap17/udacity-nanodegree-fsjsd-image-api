114018072d524ddf4635831b2551da6de7f9883b
Amol Jagtap <ajfsdeveloper@gmail.com>
2021-04-04 14:29:28 +0530

    IMAGE_API: Resize image using Sharp and send as response

--- src/routes/images/index.ts
+++ src/routes/images/index.ts
@@ -1,19 +1,30 @@
 import express from 'express'
+import sharp from 'sharp'
 import { promises as fs } from 'fs'
 import { imageParamsValidator } from '../../middlewares'
 
 const imagesRoutes = express.Router()
 
 imagesRoutes.get('/', imageParamsValidator, async (req, res) => {
-  const file = 'src/assets/images/fjord.jpg'
+  const { filename, width, height } = req.query
+  const resizedImgPath = `src/assets/thumbs/${filename}_${width}_${height}.jpg`
 
-  // const img = fs.readFileSync(file)
+  const originalImage = await fs.readFile(`src/assets/images/${filename}.jpg`)
 
-  const img = await fs.readFile(file)
+  await sharp(originalImage)
+    .resize({
+      width: Number(width),
+      height: Number(height),
+      fit: sharp.fit.cover,
+      position: sharp.strategy.entropy,
+    })
+    .toFile(resizedImgPath)
+
+  const resizedImage = await fs.readFile(resizedImgPath)
 
   res.type('jpg')
 
-  res.send(img)
+  res.send(resizedImage)
 })
 
 export { imagesRoutes }
