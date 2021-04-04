2cab50ff9df87e58ef920d580251577e4a290fa7
Amol Jagtap <ajfsdeveloper@gmail.com>
2021-04-04 19:20:42 +0530

    IMAGE_API: Add query params validations

--- src/middlewares/image-params-validator.ts
+++ src/middlewares/image-params-validator.ts
@@ -1,12 +1,46 @@
 import express from 'express'
 
+interface IQueryParams {
+  filename: string
+  width: string
+  height: string
+}
+
+interface IError {
+  message: string
+  description: string
+}
+
 export const imageParamsValidator = (
   req: express.Request,
   res: express.Response,
   next: express.NextFunction
 ): void => {
-  const params = req.query
-  console.log('params : ', params)
+  const { filename, width, height } = (req.query as unknown) as IQueryParams
+
+  const message = 'Bad Request'
+  let description = ''
+  const errors: IError[] = []
+
+  if (!filename || !width || !height) {
+    description = 'Required query params not passed.'
+
+    errors.push({ message, description })
+  }
+
+  if (isNaN(Number(width)) || isNaN(Number(height))) {
+    description = 'Required width and height params are not numeric.'
+
+    errors.push({ message, description })
+  }
+
+  if (description.length > 0) {
+    res.status(400).send({
+      errors,
+    })
+
+    return
+  }
 
   next()
 }
