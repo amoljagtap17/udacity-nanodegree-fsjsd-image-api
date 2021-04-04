79242ee921187ba173003b0732017e8326e1320f
Amol Jagtap <ajfsdeveloper@gmail.com>
2021-04-04 14:05:10 +0530

    IMAGE_API: Read file asynchronously and add sharp npm package

--- src/routes/images/index.ts
+++ src/routes/images/index.ts
@@ -1,13 +1,15 @@
 import express from 'express'
-import fs from 'fs'
+import { promises as fs } from 'fs'
 import { imageParamsValidator } from '../../middlewares'
 
 const imagesRoutes = express.Router()
 
-imagesRoutes.get('/', imageParamsValidator, (req, res) => {
+imagesRoutes.get('/', imageParamsValidator, async (req, res) => {
   const file = 'src/assets/images/fjord.jpg'
 
-  const img = fs.readFileSync(file)
+  // const img = fs.readFileSync(file)
+
+  const img = await fs.readFile(file)
 
   res.type('jpg')
 
