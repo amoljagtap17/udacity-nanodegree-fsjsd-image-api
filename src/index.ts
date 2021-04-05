0bf23df052568f40ab456c4fa927c72d3da2bd5d
Amol Jagtap <ajfsdeveloper@gmail.com>
2021-04-05 20:34:26 +0530

    IMAGE_API: Create thumbs directory on application start

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express'
+import { promises as fs } from 'fs'
 import { routes } from './routes'
 
 export const app = express()
 
 const PORT = 8000
+
+const createThumbsDirectory = async () => {
+  await fs.mkdir('src/assets/thumbs', { recursive: true })
+}
+
+createThumbsDirectory()
 
 app.use(express.urlencoded({ extended: false }))
 
